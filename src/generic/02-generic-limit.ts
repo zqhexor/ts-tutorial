0f2f0b6017e675d37811871a99a8a9003c04d554
HexOr <676658978@qq.com>
2022-05-04 23:09:39 +0800

    feat: add generic limit

---
+++ src/generic/02-generic-limit.ts
@@ -0,0 +1,21 @@
+/**
+ * 泛型约束
+ *  类型变量 extends 类型
+ */
+// 定义一个接口，用来约束将来的某个类型中必须要有length这个属性
+interface ILength {
+  length: number;
+}
+
+function getLength<T extends ILength>(t: T): number {
+  return t.length;
+}
+
+console.log(getLength<string>('what are you doing'))
+// console.log(getLength<number>(3)) // error
+
+
+// 默认类型
+interface MyType<T = string> {
+  value: T;
+}
