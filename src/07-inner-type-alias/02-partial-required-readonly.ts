8f34a9e840787e437db79a0a34f0d366d06bb139
HexOr <676658978@qq.com>
2022-05-16 02:11:53 +0800

    feat: add required and reaonly

---
+++ src/07-inner-type-alias/02-partial-required-readonly.ts
@@ -0,0 +1,48 @@
+/**
+ * Partial<T> 的作用就是将某个类型里的属性全部变为可选项 ?
+ * 实现:
+      type Partial<T> = {
+        [P in keyof T]?: T[P];
+      };
+ */
+
+interface Todo {
+  title: string;
+  description: string;
+}
+
+const todo1: Partial<Todo> = {
+  description: "描述",
+};
+
+/**
+ * Required<T> 的作用就是将某个类型里的属性全部变为必选项
+ * 实现:
+      type Required<T> = {
+        [P in keyof T]-?: T[P];
+      };
+ * 以上代码中，-? 的作用就是移除可选项 ?
+ */
+
+interface Props {
+  a?: number;
+  b?: string;
+}
+
+const props1: Props = { a: 5 }; // 正确
+// const props2: Required<Props> = { a: 5 }; // 错误
+
+/**
+ * Readonly<T> 的的作用是将某个类型所有属性变为只读属性
+ * 实现:
+      type Readonly<T> = {
+        readonly [P in keyof T]: T[P];
+      };
+ */
+
+const todo3: Readonly<Todo> = {
+  title:"名字",
+  description: "描述"
+}
+
+// todo3.title = "标题" // 错误
