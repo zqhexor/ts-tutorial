3c3dbfbecf09abf2e3e54f5515b84216a8d42938
L-zengqiao <L-zengqiao@qianxin.com>
2022-06-10 10:09:04 +0800

    feat: MyOmit的一万种实现

--- src/09-challenge/00003-medium-omit/template.ts
+++ src/09-challenge/00003-medium-omit/template.ts
@@ -1,5 +1,17 @@
-type MyOmit<T, K extends keyof T> = {
-  [P in keyof T]: P extends K ? never: T[P]
+type MyOmit<T, K extends keyof T> = Pick<
+  T,
+  {
+    [P in keyof T]: P extends K ? never : P;
+  }[keyof T]
+>;
+
+// 其他方式
+type MyOmit2<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type MyOmit3<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+type MyOmit4<T, K extends keyof T> = {
+  [P in Exclude<keyof T, K>]: T[P];
 };
 
 interface Todo {
