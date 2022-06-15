bce42adc6a123b146d7a6acb6ac9b861c422e8e7
L-zengqiao <L-zengqiao@qianxin.com>
2022-06-15 18:50:13 +0800

    feat: GetReadonlyKeys

--- src/09-challenge/00005-extreme-readonly-keys/template.ts
+++ src/09-challenge/00005-extreme-readonly-keys/template.ts
@@ -1,1 +1,9 @@
-type GetReadonlyKeys<T> = any
+type IsEqual<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1: 2) ? true : false;
+
+type GetReadonlyKeys<T> = keyof {
+  [K in keyof T as IsEqual<Pick<T, K>, Readonly<Pick<T, K>>> extends true
+  ? K
+  : never]: T[K];
+};
