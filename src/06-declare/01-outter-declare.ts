7aac7f9a8033580141e23438fc3becb08f8027fc
HexOr <41619463+zqhexor@users.noreply.github.com>
2026-02-03 18:41:53 +0800

    Enhance documentation for type declarations
    Added comments explaining type declarations and module usage in TypeScript.

--- src/06-declare/01-outter-declare.ts
+++ src/06-declare/01-outter-declare.ts
@@ -6,6 +6,9 @@
  * 一般自带类型声明模块，如'query-string'；
  * 否则，则安装类型声明模块，如'lodash'，需安装"npm install @type/lodash --dev"
  * 否则，用declare
+ *
+ *  - 自动识别：如果你的 App.ts 对应一个 App.d.ts，TS 会自动关联。
+ *  - 作为桥梁：当你引用一个纯 JS 的 npm 包时，TS 全靠寻找对应的 .d.ts（无论是包内自带的，还是通过 @types/xxx 安装的）来获取提示。
  */
 
 // 声明a为一个数字
@@ -26,3 +29,12 @@ declare module 'io' {
   export function read(file: string): string;
   export function write(file: string, data: string): void;
 }
+
+// 应用场景
+// env.d.ts
+declare module "*.vue" {
+  // import { DefineComponent } from "vue";
+  // const component: DefineComponent<{}, {}, any>;
+  // export default component;
+}
+// 假设你有一个 JS 文件 utils.js, 你需要创建一个 utils.d.ts(放在同级目录或 types 目录下)
